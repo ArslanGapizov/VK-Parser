22f3ddb18b93f1afdd1625b943dccd14a6937b08
Arslan <Arslan@DESKTOP-HHBMM1H>
2016-07-28 17:55:05 +0300

    feat: Add Login form + captha

---
+++ VK Parser/API.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Diagnostics;
+
+namespace VK_Parser
+{
+    public static class API
+    {
+
+        private static HttpClient _client;
+
+        public static HttpClient Client
+        {
+            get
+            {
+                if(_client == null)
+                {
+                    _client = new HttpClient();
+                }
+                return _client;
+            }
+        }
+
+        public static string ApiDomain { get { return "vk.com"; } }
+        public static string ClientId { get { return "2274003"; } }
+        public static string ClientSecret { get { return "hHbZxrka2uZ6jB1inYsH"; } }
+        public static string Version { get { return "5.53"; } }
+        public static string Token { get; set; }
+        
+
+
+
+        public static async Task<string> Authorize(string login, string password, string captcha_sid, string captcha_key)
+        {
+            string requestUri = string.Format("https://oauth.vk.com/token?grant_type=password&client_id={0}&client_secret={1}&username={2}&password={3}&v={4}&2fa_supported=1",ClientId, ClientSecret, login, password, Version);
+            if (captcha_key != null)
+                requestUri += string.Format("&captcha_sid={0}&captcha_key={1}", new object[2] { (object)captcha_sid, (object)captcha_key });
+
+            return await (await GetAsync(requestUri)).Content.ReadAsStringAsync();
+        }
+
+        public static async Task<HttpResponseMessage> GetAsync(string requestUri)
+        {
+            try
+            {
+                return await Client.GetAsync(requestUri);
+            }
+            catch(Exception ex)
+            {
+                    Debug.WriteLine(ex.Message);
+                    return null;
+            }
+        }
+    }
+}
--- VK Parser/MainWindow.xaml.cs
+++ VK Parser/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
 
 namespace VK_Parser
 {
@@ -20,9 +23,86 @@ namespace VK_Parser
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string CaptchaSid { get; set; }
+
         public MainWindow()
         {
+
             InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginInterfaceChanges();
+            JObject AuthResponse = new JObject();
+            AuthResponse = JObject.Parse(await API.Authorize(textLogin.Text, textPassword.Password, CaptchaSid, textCaptcha.Text));
+
+            lbLoginError.Visibility = Visibility.Collapsed;
+            lbLoginError.Content = "";
+            imageCaptcha.Visibility = Visibility.Collapsed;
+            textCaptcha.Visibility = Visibility.Collapsed;
+            textCaptcha.Text = "";
+            CaptchaSid = null;
+
+            JToken token;
+            if (AuthResponse.TryGetValue("access_token", out token))
+            {
+                API.Token = AuthResponse["access_token"].ToString();
+                return;
+            }
+            if (AuthResponse.TryGetValue("error", out token) && AuthResponse["error"].ToString() == "need_captcha")
+            {
+                CaptchaSid = AuthResponse["captcha_sid"].ToString();
+                var captchaImgPath = AuthResponse["captcha_img"].ToString();
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(captchaImgPath, UriKind.Absolute);
+                bitmap.EndInit();
+
+                imageCaptcha.Source = bitmap;
+                
+
+                lbLoginError.Visibility = Visibility.Visible;
+                lbLoginError.Content = AuthResponse["error"].ToString();
+                imageCaptcha.Visibility = Visibility.Visible;
+                textCaptcha.Visibility = Visibility.Visible;
+            }
+            else if (AuthResponse.TryGetValue("error", out token) && AuthResponse["error"].ToString() == "invalid_client")
+            {
+                lbLoginError.Visibility = Visibility.Visible;
+                lbLoginError.Content = AuthResponse["error"].ToString();
+            }
+            else
+            {
+                MessageBox.Show(AuthResponse.ToString());
+            }
+
+            LogoutInterfaceChanges();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            API.Token = "";
+            //TODO: Kill threads
+            LogoutInterfaceChanges();
+        }
+
+        private void LoginInterfaceChanges()
+        {
+            textLogin.IsEnabled = false;
+            textPassword.IsEnabled = false;
+            checkRemember.IsEnabled = false;
+            btnLogin.IsEnabled = false;
+            btnLogout.IsEnabled = true;
+        }
+        private void LogoutInterfaceChanges()
+        {
+            textLogin.IsEnabled = true;
+            textPassword.IsEnabled = true;
+            checkRemember.IsEnabled = true;
+            btnLogin.IsEnabled = true;
+            btnLogout.IsEnabled = false;
         }
     }
 }
